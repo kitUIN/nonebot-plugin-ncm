2377246b65a8f3dc2e2671ec9a66d68566a1f17c
student_2333 <lgc2333@126.com>
2023-02-10 03:13:33 +0800

    adjust

--- nonebot-plugin-ncm/__init__.py
+++ nonebot-plugin-ncm/__init__.py
@@ -20,8 +20,14 @@ from .data_source import nncm, ncm_config, setting, Q, cmd
 __plugin_meta__ = nonebot.plugin.PluginMetadata(
     name='网易云无损音乐下载',
     description='✨ 基于go-cqhttp与nonebot2的 网易云 无损音乐下载 ✨',
-    usage=f'''将网易云歌曲/歌单分享到群聊即可自动解析\n回复分享消息+文字`下载`即可自动下载(需要时间)\n
-            {cmd}ncm t:开启下载\n{cmd}ncm t:关闭下载\n{cmd}点歌 歌名:点歌''',
+    usage=(
+        '将网易云歌曲/歌单分享到群聊即可自动解析\n'
+        '回复分享消息 + 文字`下载` 即可开始下载歌曲并上传到群文件(需要稍等一会)\n'
+        '指令：\n'
+        f'开启下载：{cmd}ncm t\n'
+        f'关闭下载：{cmd}ncm f\n'
+        f'点歌：{cmd}点歌 歌名'
+    ),
     extra={'version': '1.5.0'}
 )
 
